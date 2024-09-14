32238155e8a79ec899a65c3d39a6e20178e4fca1
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2024-09-13 22:56:35 -0600

    feat: solve nearest repeated entries (hash tables)

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -2,13 +2,32 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class NearestRepeatedEntries {
   @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
   public static int findNearestRepetition(List<String> paragraph) {
-    // TODO - you fill in here.
-    return 0;
+//    Time O(N); Space O(d) -> number of distinct entries in the array
+    int closestPairDistance = -1;
+    Map<String, Integer> stringIndexOccurrence = new HashMap<>();
+
+    for (int i = 0; i < paragraph.size(); i++) {
+      String key = paragraph.get(i);
+      if (stringIndexOccurrence.containsKey(key)) {
+        int distance = i - stringIndexOccurrence.get(key);
+
+        if (distance < closestPairDistance || closestPairDistance < 0) {
+          closestPairDistance = distance;
+        }
+      }
+
+      stringIndexOccurrence.put(key, i);
+    }
+
+    return closestPairDistance;
   }
 
   public static void main(String[] args) {
