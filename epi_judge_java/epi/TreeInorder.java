23e451f356d9cc46cf2ce077d2bd5c11463d0a91
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2024-11-27 20:08:41 -0600

    feat: postorder traversal

--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -50,68 +50,89 @@ public class TreeInorder {
 
 
   // Variant 1: Preorder traversal
-  public static List<Integer> preorderTraversal(BinaryTreeNode<Integer> tree) {
-    // Time O(n) Space O(N + H)
-    // Same as before, but with the difference that:
-    // - we pop the stack on every iteration
-    // - quee first the right tree; then the left
-    // - if left != null, then current = left; otherwise, right
-    // - if a tree has no left or right subtrees, current becomes the next element
-    // in the stack
-    Deque<BinaryTreeNode<Integer>> nodeStack = new ArrayDeque<>();
-    List<Integer> preorderTraversalList = new ArrayList<>();
-
-    BinaryTreeNode<Integer> current;
-    if (tree != null) {
-      nodeStack.add(tree);
-    }
-
-    while (!nodeStack.isEmpty()) {
-      current = nodeStack.removeLast();
-      preorderTraversalList.add(current.data);
-
-      if (current.right != null) {
-        nodeStack.add(current.right);
-      }
-      if (current.left != null) {
-        nodeStack.add(current.left);
-      }
-    }
-
-    return preorderTraversalList;
-  }
+//  public static List<Integer> preorderTraversal(BinaryTreeNode<Integer> tree) {
+//    // Time O(n) Space O(N + H)
+//    // Same as before, but with the difference that:
+//    // - we pop the stack on every iteration
+//    // - quee first the right tree; then the left
+//    // - if left != null, then current = left; otherwise, right
+//    // - if a tree has no left or right subtrees, current becomes the next element
+//    // in the stack
+//    Deque<BinaryTreeNode<Integer>> nodeStack = new ArrayDeque<>();
+//    List<Integer> preorderTraversalList = new ArrayList<>();
+//
+//    BinaryTreeNode<Integer> current;
+//    if (tree != null) {
+//      nodeStack.add(tree);
+//    }
+//
+//    while (!nodeStack.isEmpty()) {
+//      current = nodeStack.removeLast();
+//      preorderTraversalList.add(current.data);
+//
+//      if (current.right != null) {
+//        nodeStack.add(current.right);
+//      }
+//      if (current.left != null) {
+//        nodeStack.add(current.left);
+//      }
+//    }
+//
+//    return preorderTraversalList;
+//  }
 
   // Variant 2: Postorder traversal
   public static List<Integer> postorderTraversal(BinaryTreeNode<Integer> tree) {
     // Time O(n) Space O(N + H)
-    // Same as before, but with the difference that:
-    // - we pop the stack on every iteration
-    // - quee first the right tree; then the left
-    // - if left != null, then current = left; otherwise, right
-    // - if a tree has no left or right subtrees, current becomes the next element
-    // in the stack
-    Deque<BinaryTreeNode<Integer>> nodeStack = new ArrayDeque<>();
-    List<Integer> preorderTraversalList = new ArrayList<>();
+    // store each node with its visited status
+    // initially all nodes are added to the stack with visited set to false
+    // Add root to stack
+    // on each iteration do this:
+    // set current to the last element in the stack (don't pop yet!) and do this:
+    // if current node's status == true OR current has no children
+    //    then pop the last element in stack
+    //    and set current to next element in stack
+    // else
+    //    add right tree to stack, and then the left tree
+    //    set current to last element without popping
+    Deque<NodeAndVisitedState> nodeStack = new ArrayDeque<>();
+    List<Integer> postorderTraversalList = new ArrayList<>();
 
-    BinaryTreeNode<Integer> current;
+    NodeAndVisitedState current;
     if (tree != null) {
-      nodeStack.add(tree);
+      nodeStack.add(new NodeAndVisitedState(tree, false));
     }
 
     while (!nodeStack.isEmpty()) {
-      current = nodeStack.removeLast();
-      preorderTraversalList.add(current.data);
+      current = nodeStack.peekLast();
 
-      if (current.right != null) {
-        nodeStack.add(current.right);
-      }
-      if (current.left != null) {
-        nodeStack.add(current.left);
+      if (current.visited || (current.node.left == null && current.node.right == null)) {
+        postorderTraversalList.add(nodeStack.removeLast().node.data);
+      } else {
+        if (current.node.right != null) {
+          nodeStack.add(new NodeAndVisitedState(current.node.right, false));
+        }
+        if (current.node.left != null) {
+          nodeStack.add(new NodeAndVisitedState(current.node.left, false));
+        }
+
+        // we mark it as visited after adding current's children to the stack
+        current.visited = true;
       }
     }
 
-    return preorderTraversalList;
+    return postorderTraversalList;
   }
+
+  private static class NodeAndVisitedState {
+    public BinaryTreeNode<Integer> node;
+    public Boolean visited;
+    public NodeAndVisitedState(BinaryTreeNode<Integer> node, Boolean visited) {
+      this.node = node;
+      this.visited = visited;
+    }
+  }
+
 
   public static void main(String[] args) {
     System.exit(
